2cef7111bd703e08803a29e9f534cde76470a19e
niteshkumar8848 <niteshkumarlodh@gmail.com>
2026-04-16 22:07:27 +0530

    css update

--- gorent-frontend/src/pages/AboutUs.js
+++ gorent-frontend/src/pages/AboutUs.js
@@ -107,7 +107,6 @@ function AboutUs() {
           background:linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('https://images.unsplash.com/photo-1603584173870-7f23fdae1b7a?q=80&w=869&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
           background-size: cover;
           background-position: center;
-          background-filter: brightness(0.1);
           color: #e8e8e8;
           text-align: center;
         }
